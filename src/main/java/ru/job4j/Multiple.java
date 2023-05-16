7aba599df2eb5866522ed825fd147ada486128e8
Ilya Galaktionov <igalaktionov1@gmail.com>
2023-05-16 22:21:06 +0300

    6. Checkstyle [#4123]

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -2,11 +2,11 @@ package ru.job4j;
 
 public class Multiple {
     public static void main(String[] args) {
-        int first_number = 1;
+        int firstNumber = 1;
         int result;
         for (int i = 2; i <= 9; i++) {
-            result = first_number * i;
-            System.out.println(first_number + " * " + i + " = " + result);
+            result = firstNumber * i;
+            System.out.println(firstNumber + " * " + i + " = " + result);
         }
     }
 }
