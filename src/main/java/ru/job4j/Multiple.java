d54a76b1eead7f3eff4cbe2c09195681d0ec46d5
Ilya Galaktionov <igalaktionov1@gmail.com>
2023-05-18 21:55:02 +0300

    4.1. Разбор первой программы. [#159033]

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -3,9 +3,8 @@ package ru.job4j;
 public class Multiple {
     public static void main(String[] args) {
         int firstNumber = 1;
-        int result;
         for (int i = 2; i <= 9; i++) {
-            result = firstNumber * i;
+            int result = firstNumber * i;
             System.out.println(firstNumber + " * " + i + " = " + result);
         }
     }
